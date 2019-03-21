9cad18ea9af7e7e25d5c816dd6000df737ab2ed7
Paulo Roberto Goncalves Da Silveira Junior <181710055@senacinformatica.br>
2019-03-21 10:59:38 -0300

    atividade site css samsung

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Samsung Brasil |</title>
+        <link href="estilo.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+       <div id="div01"> </div>
+       <ul id="menuTopo">
+           <li><a href="#">Eletrodomésticos</a></li>
+           <li><a href="#">Informática</a></li>
+           <li><a href="#">Loja Online</a></li>
+       </ul>
+       <ul id="menuTopo2">   
+            <li><a href="#">Promoção</a></li>
+            <li><a href="#">Suporte</a></li>
+       </ul>
+       
+       <div id="store"><a href="#"><img src="imagens/search.png" alt=""/></a></div>
+        <div id="search"><a href="#"><img src="imagens/store.png" alt=""/></a></div>
+       
+        <a class="logo" href="#"><img src="imagens/samsung_logo_mini.png" alt=""/></a>
+        <a class="fotos" href="#"><img src="imagens/fotos.png" alt=""/></a>
+        <h1 id="texto">Descubra o Galaxy</h1>
+        
+        <div id="banner"></div>
+        
+        
+        
+    </body>
+</html>
